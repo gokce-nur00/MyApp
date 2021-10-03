0bbbc5fb126743d8336381a5fb7d3e1875959449
gokce-nur00 <gokcepulat00@gmail.com>
2021-10-03 22:11:13 +0300

    Set the port number from the environment variable

--- src/main/java/MyApp/App.java
+++ src/main/java/MyApp/App.java
@@ -46,7 +46,10 @@ public class App {
     public static void main(String[] args) {
 
         Logger logger = LogManager.getLogger(App.class);
-        logger.error("Current port number");
+        int port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        logger.error("Current port number:" + port);
+
 
         port(getHerokuAssignedPort());
 
